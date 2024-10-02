c4f518c0ae0c07b9f27ec186959e989906f39c65
ATOROYO <davidatoroyosika@gmail.com>
2024-10-02 23:14:34 +0300

    set the route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello fromthe server side');
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
