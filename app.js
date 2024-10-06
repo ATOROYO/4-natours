429e654f71fbfbd47fc93a1a7528719d6c38e970
ATOROYO <davidatoroyosika@gmail.com>
2024-10-06 19:28:43 +0300

    Wrote get method to get only one tour

--- app.js
+++ app.js
@@ -30,6 +30,19 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+// Getting on;y one tour
+app.get('/api/v1/tours/:5', (req, res) => {
+  const id = res.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
