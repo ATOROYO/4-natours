0b483e5e88c8220a3d1636dd0ca30ed0154aa699
ATOROYO <davidatoroyosika@gmail.com>
2025-02-20 11:58:20 +0300

    Refactored the createTour function

--- app.js
+++ app.js
@@ -39,11 +39,7 @@ const getTour = (req, res) => {
   });
 };
 
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTour);
-
-// Posting to the database
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -64,7 +60,11 @@ app.post('/api/v1/tours', (req, res) => {
   );
 
   // res.send('Done');
-});
+};
+
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTour);
+app.post('/api/v1/tours');
 
 // The pacth method
 app.patch('/api/v1/tours/:id', (req, res) => {
