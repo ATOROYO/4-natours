0e3d2c73b59f51b936f72ba7c6bd976bd703498b
ATOROYO <davidatoroyosika@gmail.com>
2025-02-21 16:01:47 +0300

    Add placeholder for getUser route with error response

--- app.js
+++ app.js
@@ -116,6 +116,13 @@ const createUser = (req, res) => {
   });
 };
 
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
 // app.post('/api/v1/tours', createTour);
