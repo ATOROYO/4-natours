744c8ff3d391aa0f9415d9feb2f12b444df0e38d
ATOROYO <davidatoroyosika@gmail.com>
2025-02-21 16:05:47 +0300

    Add placeholder for deleteUser route with error response

--- app.js
+++ app.js
@@ -130,6 +130,13 @@ const updateUser = (req, res) => {
   });
 };
 
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
 // app.post('/api/v1/tours', createTour);
