74c13256ed7cba58eb15fdc363851a5921502c09
ATOROYO <davidatoroyosika@gmail.com>
2025-02-22 10:27:46 +0300

    Remove commented-out tour route handlers for improved code clarity

--- app.js
+++ app.js
@@ -26,12 +26,6 @@ const tours = JSON.parse(
 
 // 2) ROUTE HANDLERS
 
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
