c7984b9288c53fc4523a4ae262d1ed8d78a8d2ab
ATOROYO <davidatoroyosika@gmail.com>
2025-02-22 10:36:41 +0300

    Refactor app structure by integrating tour and user routers for improved organization

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -19,12 +21,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-// 2) ROUTE HANDLERS
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
