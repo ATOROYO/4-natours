6397810dfa272e49712bd1af38b0dee0cea645cf
ATOROYO <davidatoroyosika@gmail.com>
2025-03-01 01:13:57 +0300

    Remove unused File System module import from tourController

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,4 @@
-const fs = require('fs'); // File System Module
-const Tour = require('./../models/tourModel');
+const Tour = require('../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
