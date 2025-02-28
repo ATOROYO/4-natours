1a7c28e178eab564d2a514c3b6e24a7060489036
ATOROYO <davidatoroyosika@gmail.com>
2025-02-28 23:10:04 +0300

    Refactor tour schema and model into tourModel.js

---
+++ models/tourModel.js
@@ -0,0 +1,19 @@
+// The schema for the data validation
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// The Model for the schema
+const Tour = mongoose.model('Tour', tourSchema);
--- server.js
+++ server.js
@@ -18,26 +18,6 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-// The schema for the data validation
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-// The Model for the schema
-const Tour = mongoose.model('Tour', tourSchema);
-
 const testTour = new Tour({
   name: 'The Forest Hiker',
   rating: 4.7,
