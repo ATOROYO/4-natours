36c33c5da6b0e0a02f63011b610d05e5b6065a71
ATOROYO <davidatoroyosika@gmail.com>
2025-03-01 12:40:13 +0300

    Implement createTour function to save new tour data and handle errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,13 +27,25 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'Success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save()
+
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'Success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data set',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
