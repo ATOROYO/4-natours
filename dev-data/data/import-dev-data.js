1763f7bc31ac97df7dbe4fb42a7e34762b94e9ab
ATOROYO <davidatoroyosika@gmail.com>
2025-03-03 16:18:44 +0300

    Add script to import development data and establish database connection

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// Connect to the database
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
