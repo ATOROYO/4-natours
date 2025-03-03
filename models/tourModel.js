ed64a60786aa46af2c8584977da92d5b6cf6a38e
ATOROYO <davidatoroyosika@gmail.com>
2025-03-03 15:55:33 +0300

    Add createdAt field to tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -47,6 +47,11 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a cover image'],
   },
   images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
 });
 
 // The Model for the schema
