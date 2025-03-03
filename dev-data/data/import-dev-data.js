b423f1e6dc0c628e3aa47f48ec4fbbba9c29c0dd
ATOROYO <davidatoroyosika@gmail.com>
2025-03-03 17:30:03 +0300

    Add command-line options for data import and deletion

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -45,3 +45,9 @@ const deleteData = async () => {
   }
   process.exit();
 };
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
