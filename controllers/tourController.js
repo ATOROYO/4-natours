55662bc85214601336ec35e738fc4ca4d40a463d
ATOROYO <davidatoroyosika@gmail.com>
2025-03-04 11:31:27 +0300

    Enhance tour retrieval with advanced filtering and sorting capabilities

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,17 +4,24 @@ exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
     // Build the query
-    // Filtering
+    // 1a) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // Advanced filtering
+    // 1b) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // { difficulty: 'easy', duration: { $gte: 5 } }
 
