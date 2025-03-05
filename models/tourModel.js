c84ed5b9f4e5131937446c7e634727818fca67a1
ATOROYO <davidatoroyosika@gmail.com>
2025-03-06 01:15:14 +0300

    Add slug field and implement slugify in tour model pre-save middlewareg

--- models/tourModel.js
+++ models/tourModel.js
@@ -10,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have  a duration'],
@@ -65,9 +66,20 @@ tourSchema.virtual('durationWeeks').get(function () {
 });
 
 // Document middleware: runs before .save() and .create()
-tourSchema.pre('save', function () {
-  console.log(this);
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 // The Model for the schema
 const Tour = mongoose.model('Tour', tourSchema);
