b08b145f21320f70e6b0a292187a2e4a5ad0c4c0
ATOROYO <davidatoroyosika@gmail.com>
2025-03-10 23:56:36 +0300

    Wrote code to Handle Duplicate Database Fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,15 @@
 const AppError = require('./../utils/appError');
 
-const handleCasrErrorDB = (err) => {
+const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg(/([" '])(\\?.)*?\1/);
+  console.log(value);
+  const message = `Duplicate field value: {value}. Please use another value`;
+
   return new AppError(message, 400);
 };
 
@@ -41,7 +49,8 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
-    if (error.name === 'CastError') error = handleCasrErrorDB(error);
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     sendErrorProd(error, res);
   }
 };
