8563da7325e1474cb4253a1baafe19ed4bf7820a
ATOROYO <davidatoroyosika@gmail.com>
2025-03-10 17:01:35 +0300

    Did Errors During Development vs Production

--- utils/appError.js
+++ utils/appError.js
@@ -1,10 +1,11 @@
+// Description: This file contains the error handling class for the application.
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
 
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-    this.isOperational = true;
+    this.isOperational = true; // This property is used to determine if the error is operational or not.
 
     Error.captureStackTrace(this, this.constructor);
   }
