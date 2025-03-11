3b8f5123b23361e08b176db7cdb8bc58e18a84be
ATOROYO <davidatoroyosika@gmail.com>
2025-03-11 09:54:07 +0300

    Wrote code to handle Errors Outside Express Unhandled Rejections

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ mongoose
 
 // STARTS SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}....`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLER REJECTION 💥 shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
