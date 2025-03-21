a602a97b382189387b378c2eaecb7b9071840fcf
ATOROYO <davidatoroyosika@gmail.com>
2025-03-21 16:22:23 +0300

    Wrote code for Modelling Tour Guides Embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 
 // The schema for the data validation
 const tourSchema = new mongoose.Schema(
@@ -119,6 +120,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 // Document middleware: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
