e381d1c148f3d08eed8dde278fa0d47ad469cd32
ATOROYO <davidatoroyosika@gmail.com>
2025-03-31 14:49:59 +0300

    First Steps with Pug

--- app.js
+++ app.js
@@ -70,7 +70,9 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base', { title: 'Home' });
+  res
+    .status(200)
+    .render('base', { tour: 'Maracha Mystical Trails', user: 'Dave' });
 });
 
 app.use('/api/v1/tours', tourRouter);
